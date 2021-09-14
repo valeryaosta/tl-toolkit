39eba3c4976c66224588e7b594e213eacad0f0f6
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-15 00:21:09 +0300

    tasks-reducer tests

--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -19,8 +19,8 @@ const slice = createSlice({
                 tasks.splice(index, 1);
             }
         },
-        addTaskAC(state, action: PayloadAction<{ task: TaskType }>) {
-            state[action.payload.task.todoListId].unshift(action.payload.task);
+        addTaskAC(state, action: PayloadAction<TaskType>) {
+            state[action.payload.todoListId].unshift(action.payload);
         },
         updateTaskAC(state, action: PayloadAction<{ taskId: string, model: UpdateDomainTaskModelType, todolistId: string }>) {
             const tasks = state[action.payload.todolistId];
@@ -75,7 +75,7 @@ export const addTaskTC = (title: string, todolistId: string) => (dispatch: Dispa
         .then(res => {
             if (res.data.resultCode === 0) {
                 const task = res.data.data.item
-                const action = addTaskAC({task})
+                const action = addTaskAC((task))
                 dispatch(action)
                 dispatch(setAppStatusAC({status: 'succeeded'}))
             } else {
